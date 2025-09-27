919e48b7e5e13e8e8809be2a10da4aa2b87dadcd
CodeFreak2186 <sanskaarundale@gmail.com>
2025-09-27 14:45:26 +0530

    app-90-95

--- app/app/_layout.jsx
+++ app/app/_layout.jsx
@@ -7,7 +7,9 @@ import { StatusBar } from "expo-status-bar";
 export default function RootLayout() {
   return (
     <ClerkProvider tokenCache={tokenCache}>
+      <SafeScreen>
         <Slot />
+      </SafeScreen>
       <StatusBar style="dark" />
     </ClerkProvider>
   );
---
+++ app/constants/api.js
@@ -0,0 +1,1 @@
+API_URL="https://my-expense-tracker-vi5v.onrender.com/api"
