89742aec38c8b0efdfbb38a0ebd860beaa8a5430
CodeFreak2186 <sanskaarundale@gmail.com>
2025-09-27 14:21:07 +0530

    cron-job

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,11 +3,20 @@ import dotenv from "dotenv";
 import { initDB} from "./config/db.js";
 import rateLimiter from "./middleware/rateLimiter.js";
 import transactionsRoutes from "./routes/transactionsRoutes.js";
+import job from "./config/corn.js";
+
+
+
 
 
 dotenv.config();
 
 const app = express(); 
+
+if (process.env.NODE_ENV === "production") {
+  job.start();
+}
+
 const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -18,7 +27,9 @@ app.use(rateLimiter);
 
 
 app.use("/api/transactions", transactionsRoutes);
-
+app.get("/api/check", (req, res) => {
+  res.status(200).send("API is running...");
+});
 
 
 
